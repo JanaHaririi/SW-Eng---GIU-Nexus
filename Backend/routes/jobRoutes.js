8a3549e808c4d8ae3f8b28be3e619f47be10c510
linaabdelhamid <linaabdelhamid18@gmail.com>
2026-04-30 16:17:53 +0300

    jobRoutes

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -6,20 +6,17 @@ const {
   getSavedJobs
 } = require('../controllers/jobController');
 
-// const { protect, authorize } = require('../middlewares/auth');
+const { protect, authorize } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get('/', getJobs);
 
-// TODO: add auth later
-// router.get('/saved', protect, authorize('jobSeeker'), getSavedJobs);
-router.get('/saved', getSavedJobs);
+// must be before /:id
+router.get('/saved', protect, authorize('jobSeeker'), getSavedJobs);
 
 router.get('/:id', getJobById);
 
-// TODO: add auth later
-// router.post('/:id/save', protect, authorize('jobSeeker'), toggleSaveJob);
-router.post('/:id/save', toggleSaveJob);
+router.post('/:id/save', protect, authorize('jobSeeker'), toggleSaveJob);
 
 module.exports = router;
