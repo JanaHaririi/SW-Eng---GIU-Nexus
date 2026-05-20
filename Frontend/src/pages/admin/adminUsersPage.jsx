341058ab7d17a1a1719d77e4c0c1aa86755bf9ff
JanaHaririi <jana.elharirii004@gmail.com>
2026-05-20 15:39:55 +0300

    fix: correct import path for Modal component

--- Frontend/src/pages/admin/adminUsersPage.jsx
+++ Frontend/src/pages/admin/adminUsersPage.jsx
@@ -4,7 +4,7 @@ import {
   updateUserStatus,
   deleteUser,
 } from "../../services/userService";
-import Modal from "../../components/Modal";
+import Modal from "../../components/modal";
 
 const AdminUsersPage = () => {
   const [users, setUsers] = useState([]);
